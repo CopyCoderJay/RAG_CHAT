8f5a051be186688ec01ec197972bdbf8b53187e8
CopyCoderJay <sanjayoffcial07@gmail.com>
2025-11-10 09:49:16 +0530

    UI10 Commit

--- rag_chatbot/urls.py
+++ rag_chatbot/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from apps.chat import views as chat_views
 
 urlpatterns = [
@@ -13,6 +14,14 @@ urlpatterns = [
     path('chat/', include('apps.chat.urls')),
     path('documents/', include('apps.documents.urls')),
     path('api/', include('apps.chat.api_urls')),
+    path('manifest.webmanifest', TemplateView.as_view(
+        template_name='manifest.webmanifest',
+        content_type='application/manifest+json'
+    ), name='pwa-manifest'),
+    path('service-worker.js', TemplateView.as_view(
+        template_name='service-worker.js',
+        content_type='application/javascript'
+    ), name='service-worker'),
     # Public landing page for unauthenticated users
     path('', chat_views.landing_view, name='landing'),
 ]
---
+++ templates/service-worker.js
@@ -0,0 +1,54 @@
+{% load static %}
+const CACHE_NAME = 'rag-chat-pwa-v1';
+const CORE_ASSETS = [
+  '/',
+  '/chat/',
+  '{% static "css/chatgpt.css" %}',
+  '{% static "js/chat.js" %}'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  const url = new URL(event.request.url);
+
+  if (
+    url.origin !== location.origin ||
+    url.pathname.startsWith('/api/') ||
+    url.pathname.startsWith('/admin/') ||
+    url.pathname.endsWith('.map')
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
